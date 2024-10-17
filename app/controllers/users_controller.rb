3eee378e8e5c92c05136af7d3b562af4cc3db267
Ferdinand Niedermann <113440+nerdinand@users.noreply.github.com>
2024-10-17 20:01:48 +0200

    Add names to users (#9)
    * Add names to users
    
    * Add i18n-tasks and do some translations
    
    * Configure i18n-tasks
    
    ---------
    
    Co-authored-by: Ferdinand Niedermann <nerdinand@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email)
+    params.require(:user).permit(:email, :first_name, :last_name, :nick_name)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validate :roles_must_be_valid
 
   passwordless_with :email
@@ -10,10 +12,16 @@ class User < ApplicationRecord
     Roles.new(roles)
   end
 
+  def full_name
+    nick_name_part = ("\"#{nick_name}\"" if nick_name.present?)
+
+    [first_name, nick_name_part, last_name].compact.join(' ')
+  end
+
   private
 
   def roles_must_be_valid
-    return if roles.all? { |v| Roles::ROLES.include? v } && roles.uniq == values
+    return if roles.all? { |r| Roles::ROLES.include? r } && roles.uniq == roles
 
     errors.add :roles, I18n.t('roles.validation.message')
   end
