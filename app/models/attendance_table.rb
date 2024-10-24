ffeb84ffd5d1a6b3eaed81bb2869310580f29a0c
Ferdinand Niedermann <nerdinand@users.noreply.github.com>
2024-10-24 18:28:56 +0200

    Optimise attendance table performance

--- app/models/attendance_table.rb
+++ app/models/attendance_table.rb
@@ -2,15 +2,15 @@
 
 class AttendanceTable
   def initialize(attendances, users, calendar_events)
-    @attendances = attendances
     @users = users
     @calendar_events = calendar_events
 
-    @index = @attendances.to_a.group_by(&:user_id)
+    @index = attendances.group_by { |a| "#{a.user_id}-#{a.calendar_event_id}" }
   end
 
-  attr_reader :attendances, :users, :calendar_events
+  attr_reader :users, :calendar_events
 
   def attendance_for(user, calendar_event)
+    @index["#{user.id}-#{calendar_event.id}"]&.first
   end
 end
