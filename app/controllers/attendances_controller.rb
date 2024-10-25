3b29610966b48ba6bd43ef6deb064524081fcf35
Ferdinand Niedermann <nerdinand@users.noreply.github.com>
2024-10-25 19:42:47 +0200

    Fix time frame for attendances

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class AttendancesController < ApplicationController
-  PAST_EVENTS_LIMIT = 20
+  PAST_EVENTS_FROM = 6.months
+  PAST_EVENTS_TO = 1.day
 
   def index
     users = authorize User.all
-    calendar_events = authorize CalendarEvent.past.limit(PAST_EVENTS_LIMIT)
+    calendar_events = authorize CalendarEvent.past.where(starts_at: PAST_EVENTS_FROM.ago..PAST_EVENTS_TO.from_now)
     attendances = authorize Attendance.where(user: users).where(calendar_event: calendar_events)
     @attendance_table = AttendanceTable.new(attendances, users, calendar_events)
   end
