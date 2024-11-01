a48ff69d425827f1e0d77dee3b7df0ed18a9f82a
Ferdinand Niedermann <nerdinand@users.noreply.github.com>
2024-11-01 14:58:49 +0100

    Some feature specs

---
+++ spec/features/complete_profile_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require_relative 'log_in_helpers'
+
+RSpec.describe('Completing a profile') do
+  fixtures :users
+
+  scenario do # rubocop:disable RSpec/ExampleLength
+    log_in_with_magic_link
+    expect(page).to have_content(
+      'Dein Benutzerprofil ist unvollständig. Bitte hilf uns indem du die fehlenden Informationen einträgst.'
+    )
+
+    click_on 'Benutzerprofil vervollständigen'
+    expect(page).to have_content('Benutzer bearbeiten')
+
+    fill_in 'Spitzname', with: 'Ferdi'
+    fill_in 'Anrede', with: 'Herr'
+    fill_in 'Strasse', with: 'Teststrasse 123'
+    fill_in 'PLZ', with: '4567'
+    fill_in 'Ort', with: 'Musterstadt'
+    fill_in 'Telefonnummer (mobil)', with: '+41761234567'
+    fill_in 'Geburtsdatum', with: '1970-01-01'
+    select 'Bass 2', from: 'Stimme'
+    click_on 'Benutzer aktualisieren'
+
+    expect(page).to have_content('Benutzer erfolgreich geändert.')
+    expect(page).to have_content('Ferdinand "Ferdi" Niedermann')
+  end
+end
---
+++ spec/features/create_attendance_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+def create_ongoing_event!
+  CalendarEvent.create!(
+    starts_at: 3.days.ago,
+    ends_at: 1.hour.from_now,
+    uid: 'random',
+    event_created_at: Time.zone.now,
+    summary: 'my test event'
+  )
+end
+
+RSpec.describe('Creating an attendance') do
+  fixtures :users
+
+  scenario do
+    create_ongoing_event!
+
+    log_in_with_magic_link
+    expect(page).to have_content('my test event')
+    click_on 'Anwesenheit eintragen'
+    expect(page).to have_content('Anwesenheit erfolgreich eingetragen.')
+  end
+end
