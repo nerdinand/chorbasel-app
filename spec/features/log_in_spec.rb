60574355b7d4d98977b4688f42210c760062cac9
Ferdinand Niedermann <nerdinand@users.noreply.github.com>
2024-11-03 11:09:02 +0100

    Test logging out

--- spec/features/log_in_spec.rb
+++ spec/features/log_in_spec.rb
@@ -6,14 +6,18 @@ require_relative 'log_in_helpers'
 RSpec.describe('Logging in and out') do
   fixtures :users
 
-  scenario 'Logging in with the code' do
+  scenario 'with the code' do
     log_in_with_code(users(:uwe))
     expect(page).to have_content('Willkommen, Uwe!')
     expect(page).to have_content('Dashboard')
+    click_on 'Ausloggen', visible: false
+    expect(page).to have_content('Erfolgreich ausgeloggt.')
   end
 
-  scenario 'Logging in with the magic link' do
+  scenario 'with the magic link' do
     log_in_with_magic_link(users(:uwe))
     expect(page).to have_content('Dashboard')
+    click_on 'Ausloggen', visible: false
+    expect(page).to have_content('Erfolgreich ausgeloggt.')
   end
 end
