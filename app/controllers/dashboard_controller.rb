a04c8eb419ac39b0c4090255cd6338fb0740af12
Ferdinand Niedermann <nerdinand@users.noreply.github.com>
2024-12-04 22:27:16 +0100

    Refactoring

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -20,7 +20,7 @@ LIMIT 5;
 
   def show
     @calendar_events = CalendarEvent.next
-    @attendances = CalendarEvent.ongoing.map do |calendar_event|
+    @ongoing_attendances = CalendarEvent.ongoing.map do |calendar_event|
       Attendance.find_or_initialize_by(user: current_user, calendar_event:).tap do |attendance|
         attendance.status = Attendance::STATUS_ATTENDED
       end
