472e9105bfa45d39ecd37690f904e209768b5e8c
Ferdinand Niedermann <113440+nerdinand@users.noreply.github.com>
2025-01-06 07:58:01 +0100

    Remove modern browser version check (#105)
    I checked the app with an "unsupported" version (iOS 16.4) and it seemed to work, so I think we can deploy this and have people try it. Perhaps we're not actually using anything unsupported?
    
    Co-authored-by: Ferdinand Niedermann <nerdinand@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,6 @@ class ApplicationController < ActionController::Base
   include Passwordless::ControllerHelpers
   include Pundit::Authorization
 
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-
   helper_method :current_user, :logged_in?
 
   before_action :require_user!
