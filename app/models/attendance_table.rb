f908bf671c8d57085c24ce4e5d0c5b0bf7964dd9
Ferdinand Niedermann <nerdinand@users.noreply.github.com>
2025-07-29 19:37:39 +0200

    Attendances table: Add additional table headers in between registers

--- app/models/attendance_table.rb
+++ app/models/attendance_table.rb
@@ -2,13 +2,13 @@
 
 class AttendanceTable
   def initialize(attendances, users, calendar_events)
-    @users = users
     @calendar_events = calendar_events
 
     @index = attendances.group_by { |a| "#{a.user_id}-#{a.calendar_event_id}" }
+    @canonical_register_users = users.group_by { |u| u.register_object.canonical_register }
   end
 
-  attr_reader :users, :calendar_events
+  attr_reader :canonical_register_users, :calendar_events
 
   def attendance_for(user, calendar_event)
     @index["#{user.id}-#{calendar_event.id}"]&.first
