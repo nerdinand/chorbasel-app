e75a966138d1c5bcdef6064cdc3670b3f724220e
Ferdinand Niedermann <113440+nerdinand@users.noreply.github.com>
2025-07-30 22:55:12 +0200

    Atendance pagination (#172)
    Co-authored-by: Ferdinand Niedermann <nerdinand@users.noreply.github.com>

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 class AttendancesController < ApplicationController
-  EVENTS_FROM = 1.month
-  EVENTS_TO = 2.months
-
   def index
+    @attendance_pagination = AttendancePagination.new(params[:from], params[:to])
     users = User.ordered_by_register
-    calendar_events = CalendarEvent.where(starts_at: EVENTS_FROM.ago..EVENTS_TO.from_now)
+    calendar_events = CalendarEvent.where(starts_at: @attendance_pagination.current_range)
     attendances = authorize Attendance.where(user: users).where(calendar_event: calendar_events)
     @attendance_table = AttendanceTable.new(attendances, users, calendar_events)
     @excuse_requested_attendances = Attendance.excuse_requested
