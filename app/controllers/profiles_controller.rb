eeedc47dc4f2eebe63618417437f655029faca62
Ferdinand Niedermann <nerdinand@users.noreply.github.com>
2025-07-31 13:45:10 +0200

    Order Lookbook by first name

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,6 @@
 
 class ProfilesController < ApplicationController
   def index
-    @users = User.active.joins(:profile_picture_attachment)
+    @users = User.active.joins(:profile_picture_attachment).order(:first_name)
   end
 end
