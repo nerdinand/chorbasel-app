c8c235bd5a1d9ff3133a4d0a35dcf18a0d5115d3
Ferdinand Niedermann <113440+nerdinand@users.noreply.github.com>
2025-08-11 12:23:16 +0200

    Improve upcoming birthdays (#196)
    * Small improvements
    
    * Show full name for birthdays
    * Excuses: Don't use Email-style quoting
    
    * Improve upcoming birthdays
    
    * refactor query using Arel
    * use UserStatus scopes in query
    
    ---------
    
    Co-authored-by: Ferdinand Niedermann <nerdinand@users.noreply.github.com>

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,27 +1,6 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
-  UPCOMING_BIRTHDAYS_QUERY = "
-SELECT
-  users.id,
-  -- Calculate the next occurrence of the birthday in the current year or the next year
-  CASE
-    WHEN strftime('%m-%d', birth_date) >= strftime('%m-%d', 'now') THEN
-      date(strftime('%Y', 'now') || '-' || strftime('%m-%d', birth_date))
-    ELSE
-      date((strftime('%Y', 'now') + 1) || '-' || strftime('%m-%d', birth_date))
-  END AS next_birthday
-FROM
-  users
-JOIN user_statuses ON users.id = user_statuses.user_id
-WHERE
-  user_statuses.status == 'active'
-  AND user_statuses.to_date IS NULL
-  AND users.birth_date IS NOT NULL
-ORDER BY next_birthday ASC
-LIMIT 5;
-"
-
   def show
     @calendar_events = CalendarEvent.next
     @attendances = CalendarEvent.ongoing.map do |calendar_event|
@@ -36,8 +15,23 @@ LIMIT 5;
   private
 
   def upcoming_birthdays
-    ActiveRecord::Base.connection.execute(UPCOMING_BIRTHDAYS_QUERY).map do |h|
-      h.merge(user: User.find(h['id']), next_birthday: Date.parse(h['next_birthday'])).symbolize_keys
-    end
+    upcoming_birthdays_query.to_h { |e| [User.find(e.id), Date.parse(e.next_birthday)] }
+  end
+
+  def upcoming_birthdays_query
+    User.select(
+      'users.id', "
+    CASE
+      WHEN strftime('%m-%d', birth_date) >= strftime('%m-%d', 'now')
+      THEN date(strftime('%Y', 'now') || '-' || strftime('%m-%d', birth_date))
+      ELSE date((strftime('%Y', 'now') + 1) || '-' || strftime('%m-%d', birth_date))
+    END AS next_birthday"
+    ).joins(:user_statuses).where(
+      'user_statuses.id IN (?) AND users.birth_date IS NOT NULL', active_user_status_ids
+    ).order(next_birthday: :asc).limit(5)
+  end
+
+  def active_user_status_ids
+    UserStatus.not_inactive.valid_at_time(Time.zone.now).select(:id)
   end
 end
