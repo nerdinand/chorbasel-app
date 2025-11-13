e7c56590c35c2699f2cd5d9a810a0cf45b7aa141
Ferdinand Niedermann <nerdinand@users.noreply.github.com>
2025-11-13 19:48:11 +0100

    Add record timestamp tallies to statistics page

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -2,7 +2,10 @@
 
 class StatisticsController < ApplicationController
   NAME_GUESSES_TIMESPAN = 2.weeks
+  CREATED_AT_TIMESPAN = UPDATED_AT_TIMESPAN = 1.month
   LAST_N_CALENDAR_EVENTS = 5
+  TABLES_WITH_TIMESTAMPS = ActiveRecord::Base.connection.tables - %w[schema_migrations active_storage_variant_records
+                                                                     active_storage_attachments active_storage_blobs]
 
   def index
     authorize :statistics, :index?
@@ -11,6 +14,8 @@ class StatisticsController < ApplicationController
     calendar_events = CalendarEvent.unscoped.practice.order(starts_at: :desc).past
                                    .limit(LAST_N_CALENDAR_EVENTS).includes(attendances: [:user]).reverse
     @attendance_tally_table = AttendanceTallyTable.new(calendar_events)
+
+    @timestamp_tallies = timestamp_tallies
   end
 
   private
@@ -34,5 +39,25 @@ class StatisticsController < ApplicationController
       { name: 'correct', data: correct_counts },
       { name: 'incorrect', data: incorrect_counts }
     ]
+  end
+
+  def timestamp_tallies
+    [
+      { name: t('.timestamp_tallies.created'), data: timestamp_tally(CREATED_AT_TIMESPAN, 'created_at') },
+      { name: t('.timestamp_tallies.modified'), data: timestamp_tally(UPDATED_AT_TIMESPAN, 'updated_at') }
+    ]
+  end
+
+  def timestamp_tally(timespan, column)
+    min_date = timespan.ago.to_date
+
+    timestamp_occurrences = TABLES_WITH_TIMESTAMPS.flat_map do |name|
+      ActiveRecord::Base.connection.query("SELECT date(#{column}) FROM #{name} WHERE #{column} > '#{min_date}';")
+    end.flatten.tally
+
+    tally = timestamp_occurrences.transform_keys { |k| Date.parse(k) }
+
+    date_range = (min_date..Time.zone.today).to_a
+    date_range.index_with(0).merge(tally)
   end
 end
