b3b5f72cd2b982c678913b60fc99c5f37f534b64
Ferdinand Niedermann <nerdinand@users.noreply.github.com>
2025-12-15 20:51:39 +0100

    WIP

--- app/javascript/controllers/score_controller.js
+++ app/javascript/controllers/score_controller.js
@@ -6,19 +6,6 @@ export default class extends Controller {
   }
   
   connect() {
-    var osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay(this.target, {
-      autoResize: true, // just an example for an option, no option is necessary.
-      backend: "svg",
-      drawTitle: true,
-      // put further options here
-    });
-
-    var loadPromise = osmd.load("http://www.example.org/my-score.xml");
-
-    loadPromise.then(function(){
-      osmd.render();
-    });
-
     console.log("hello")
   }
 }
