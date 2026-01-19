93ebb2fa135f2417f5d107ac71f67ca3e75579d3
SK2108 <85369151+SK2108@users.noreply.github.com>
2026-01-19 20:51:07 +0100

    Add 2 more use cases for filtering songs in tests and update fixtures

--- spec/features/songs/filter_songs_spec.rb
+++ spec/features/songs/filter_songs_spec.rb
@@ -4,20 +4,30 @@ require 'rails_helper'
 require_relative '../log_in_helpers'
 
 RSpec.describe('Filtering songs') do
-  # Add expectations
   scenario do
-    # Add code here
     log_in_with_magic_link(users(:uwe))
     click_on 'Lieder'
     expect(page).to have_content('Liederlisten')
     click_on 'Alle Lieder'
-    expect(page).to have_content('Lieder')
+    expect(page).to have_content('Joyful, joyful')
+    expect(page).to have_content('Härlig är jorden')
+
     within('#language') do
       page.select 'Schwedisch'
     end
 
     click_on 'Filter anwenden'
     # Check that the page has some content
+    expect(page).to have_no_content('Joyful, joyful')
     expect(page).to have_content('Härlig är jorden')
+
+    within('#language') do
+      page.select 'English'
+    end
+
+    click_on 'Filter anwenden'
+    # Check that the page has some content
+    expect(page).to have_content('Joyful, joyful')
+    expect(page).to have_no_content('Härlig är jorden')
   end
 end
