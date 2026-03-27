b6cb908a22f351b38a19eabd77d6a00681b86522
Ferdinand Niedermann <nerdinand@users.noreply.github.com>
2026-03-27 20:26:56 +0100

    Sync calendar spec

--- spec/features/other/sync_calendar_spec.rb
+++ spec/features/other/sync_calendar_spec.rb
@@ -5,6 +5,8 @@ require_relative '../log_in_helpers'
 
 RSpec.describe 'Syncing the calendar' do
   scenario 'syncing the calendar' do
-    skip 'not implemented yet'
+    log_in_with_magic_link(users(:ferdi))
+    click_on 'Chorkalender importieren'
+    expect(page).to have_content('Chorkalender erfolgreich importiert.')
   end
 end
